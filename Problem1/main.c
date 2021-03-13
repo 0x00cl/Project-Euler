5227e2a5bae8c4249ae545112ce64a8fd4559c6b
tomas <tomas.gutierrez.l@usach.cl>
2021-03-13 20:17:09 -0300

    FIX: Problem 1: Code formatting

--- Problem1/main.c
+++ Problem1/main.c
@@ -2,9 +2,10 @@
 
 int main() {
         int total = 0;
-        for(int i = 1; i < 1000; i++)
-                if(i % 3 == 0 || i % 5 == 0)
+        for (int i = 1; i < 1000; i++) {
+                if (i % 3 == 0 || i % 5 == 0)
                         total = total + i;
+        }
 
         printf("Result: %d\n", total);
 
