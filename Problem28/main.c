8fd164bd8880c41422b114fad936f04511faa074
tomas <tomas.gutierrez.l@usach.cl>
2021-05-02 18:48:56 -0400

    ADD: Problem 28

---
+++ Problem28/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+        int total = 1;
+        int tmp = 1;
+        for (int i = 2; i < 1001; i =  i + 2) {
+            for (int j = 0; j < 4; j++) {
+                tmp = tmp + i;
+                total = total + tmp;
+            }
+        }
+
+        printf("Result: %d\n", total);
+        return 0;
+}
