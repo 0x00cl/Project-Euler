60e7bc20175da07f38059761f44d6b8f1e27d3ed
tomas <tomas.gutierrez.l@usach.cl>
2021-05-02 18:57:08 -0400

    FIX: Problem 28: Indentation space to 8 characters

--- Problem28/main.c
+++ Problem28/main.c
@@ -4,10 +4,10 @@ int main() {
         int total = 1;
         int tmp = 1;
         for (int i = 2; i < 1001; i =  i + 2) {
-            for (int j = 0; j < 4; j++) {
-                tmp = tmp + i;
-                total = total + tmp;
-            }
+                for (int j = 0; j < 4; j++) {
+                        tmp = tmp + i;
+                        total = total + tmp;
+                }
         }
 
         printf("Result: %d\n", total);
